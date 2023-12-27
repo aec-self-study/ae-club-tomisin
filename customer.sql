e0da3d9aff521b3789e9e23ab4dc60df064a368b
AllegraZarah <116184734+AllegraZarah@users.noreply.github.com>
2023-12-27 19:43:32 +0000

    Pasted the correct query from the prework

--- customer.sql
+++ customer.sql
@@ -1,5 +1,14 @@
-select date_month
-	, sum(is_active : integer) as customers
-	, sum(nrr) as nrr
-from analytics.fact_nrr
-group by 1
+select ord.customer_id
+      , customer.name
+      , customer.email
+      , min(ord.created_at) first_order_at
+      , count(ord.id) number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` customer
+left join `analytics-engineers-club.coffee_shop.orders` ord
+on customer.id = ord.customer_id
+group by ord.customer_id
+        , customer.name
+        , customer.email
+        
+order by first_order_at
+limit 10;
