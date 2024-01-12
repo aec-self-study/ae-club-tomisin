1530ed33f5318859c96510a05dce0cc1e8eddbcb
AllegraZarah <116184734+AllegraZarah@users.noreply.github.com>
2024-01-12 23:22:38 +0000

    First DAG etc.

---
+++ coffee_shop_tomisin/models/customers.sql
@@ -0,0 +1,21 @@
+-- models/users.sql
+
+{{config(
+      materialized = 'table'
+)
+}}
+
+select ord.customer_id
+      , customer.name
+      , customer.email
+      , min(ord.created_at) first_order_at
+      , count(ord.id) number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` customer
+left join `analytics-engineers-club.coffee_shop.orders` ord
+on customer.id = ord.customer_id
+group by ord.customer_id
+        , customer.name
+        , customer.email
+        
+order by first_order_at
+limit 10
