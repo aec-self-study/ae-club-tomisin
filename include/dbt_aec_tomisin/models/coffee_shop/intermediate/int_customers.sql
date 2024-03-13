e554c62423f15b321e01fde301ca468006b70350
AllegraZarah <116184734+AllegraZarah@users.noreply.github.com>
2024-03-13 08:15:38 +0000

    Reorganized my dbt project. This should inform how my YourCo project will be organized too

--- include/dbt_aec_tomisin/models/coffee_shop/intermediate/int_customers.sql
+++ include/dbt_aec_tomisin/models/coffee_shop/intermediate/int_customers.sql
@@ -3,12 +3,13 @@ select ord.customer_id
       , customer.email
       , min(ord.created_at) first_order_at
       , count(ord.id) number_of_orders
-from `analytics-engineers-club.coffee_shop.customers` customer
-left join `analytics-engineers-club.coffee_shop.orders` ord
+
+from {{ ref('stg_customers') }} customer
+left join {{ ref('stg_orders') }} ord
 on customer.id = ord.customer_id
 group by ord.customer_id
         , customer.name
         , customer.email
         
 order by first_order_at
-limit 10;
+limit 10
