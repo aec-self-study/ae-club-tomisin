6b6569d6e092474f7fd09e392e93e3f9d7866289
AllegraZarah <116184734+AllegraZarah@users.noreply.github.com>
2024-06-17 09:22:54 +0000

    CLI Calculator Exercise

--- include/aec-python/calc.py
+++ include/aec-python/calc.py
@@ -8,11 +8,19 @@ subparsers = parser.add_subparsers(help = "sub-command help", dest="command")
 
 # Add
 add = subparsers.add_parser("add", help = "add integers")
-add.add_argument("ints_to_sum", nargs=2, type=int)
+add.add_argument("ints_to_sum", nargs='*', type=int)
 
 # Subtract
-sub = subparsers.add_parser("sub", help='add integers')
+sub = subparsers.add_parser("sub", help='subtract integers')
 sub.add_argument("ints_to_sub", nargs=2, type=int)
+
+# Multiplication
+mut = subparsers.add_parser("mut", help='multiply integers')
+mut.add_argument("ints_to_mut", nargs='*', type=int)
+
+# Division
+div = subparsers.add_parser("div", help='divide integers')
+div.add_argument("ints_to_div", nargs=2, type=int)
 
  
 args = parser.parse_args()
@@ -24,3 +32,13 @@ if args.command == "add":
 if args.command == 'sub':
   our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
   print(f"The subtracted result is  {our_sub}")
+
+if args.command == 'mut':
+  our_mut = 1
+  for int in args.ints_to_mut:
+    our_mut *= int
+  print(f"The Multiplication result is  {our_mut}")
+
+if args.command == 'div':
+  our_div = args.ints_to_div[0] / args.ints_to_div[1]
+  print(f"The Division result is  {our_div}")
